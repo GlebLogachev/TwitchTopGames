b1586c9abb40580a2f26f5e4679a9fc0e8cfb39b
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-02-26 14:09:17 +0700

    added viewBinding

--- app/src/main/java/com/glogachev/twitchtopgames/view/topGames/TopGamesFragment.kt
+++ app/src/main/java/com/glogachev/twitchtopgames/view/topGames/TopGamesFragment.kt
@@ -1,23 +1,19 @@
 package com.glogachev.twitchtopgames.view.topGames
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.glogachev.twitchtopgames.App
-import com.glogachev.twitchtopgames.R
+import com.glogachev.twitchtopgames.databinding.FragmentTopGamesBinding
 import com.glogachev.twitchtopgames.view.topGames.adapter.TopGamesAdapter
-import kotlinx.android.synthetic.main.fragment_top_games.*
 
 class TopGamesFragment : Fragment() {
-
+    private lateinit var binding: FragmentTopGamesBinding
     private lateinit var viewModel: TopGamesViewModel
     private lateinit var topGamesAdapter: TopGamesAdapter
 
@@ -25,40 +21,40 @@ class TopGamesFragment : Fragment() {
         super.onCreate(savedInstanceState)
         val repository = App.appRepository
         val viewModelFactory = TopGamesViewModelFactory(repository)
-        viewModel = ViewModelProvider(this,viewModelFactory).get(TopGamesViewModel::class.java)
-
+        viewModel = ViewModelProvider(this, viewModelFactory).get(TopGamesViewModel::class.java)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_top_games, container, false)
+        binding = FragmentTopGamesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.getListOfGames()
-        viewModel.topGamesList.observe(viewLifecycleOwner, { list->
+        viewModel.topGamesList.observe(viewLifecycleOwner, { list ->
             topGamesAdapter = TopGamesAdapter(list)
-            rv_top_games.also {
+            binding.rvTopGames.also {
                 it.hasFixedSize()
                 it.adapter = topGamesAdapter
             }
         })
-        viewModel.isNetworkProblem.observe(viewLifecycleOwner,{ isNetworkProblem ->
-            network_problem_tw.isVisible = isNetworkProblem
+        viewModel.isNetworkProblem.observe(viewLifecycleOwner, { isNetworkProblem ->
+            binding.networkProblemTw.isVisible = isNetworkProblem
         })
         viewModel.isEmptyList.observe(viewLifecycleOwner, Observer { isEmptyList ->
-            empty_list_tw.isVisible = isEmptyList
+            binding.emptyListTw.isVisible = isEmptyList
         })
         viewModel.isLoading.observe(viewLifecycleOwner, Observer { isLoading ->
-            loading_pb.isVisible = isLoading
+            binding.loadingPb.isVisible = isLoading
         })
 
-        swipe_refresh_layout.setOnRefreshListener {
-            swipe_refresh_layout.isRefreshing = false
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            binding.swipeRefreshLayout.isRefreshing = false
             viewModel.getListOfGames()
         }
     }
