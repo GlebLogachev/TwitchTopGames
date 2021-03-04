05c041cbcc430ac40e670beda9f76c1641bd0321
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-03-04 12:16:45 +0700

    1) created custom retrofit CallAdapterFactory 2) added internet connection check 3) added Timber in project 4) refactor TopGamesFragment xml 5) added snackbar

--- app/src/main/java/com/glogachev/twitchtopgames/view/topGames/TopGamesViewModel.kt
+++ app/src/main/java/com/glogachev/twitchtopgames/view/topGames/TopGamesViewModel.kt
@@ -3,12 +3,14 @@ package com.glogachev.twitchtopgames.view.topGames
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.glogachev.twitchtopgames.data.retrofit.DomainThrowable
 import com.glogachev.twitchtopgames.domain.StoreResult
 import com.glogachev.twitchtopgames.domain.TopGamesRepository
 import com.glogachev.twitchtopgames.domain.model.GameDomain
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 class TopGamesViewModel(private val repository: TopGamesRepository) : ViewModel() {
     private var disposable: Disposable? = null
@@ -24,6 +26,11 @@ class TopGamesViewModel(private val repository: TopGamesRepository) : ViewModel(
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { gamesNetworkResult ->
+                (gamesNetworkResult as? StoreResult.Error)?.let {
+                    (it.error as? DomainThrowable)?.let { error ->
+                        Timber.d(error.errorModelNW.toString())
+                    }
+                }
                 _listGamesState.value = gamesNetworkResult
             }
     }
