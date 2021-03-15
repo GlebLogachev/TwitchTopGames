b4e7e855f8f44202b91cf81151a1ef52bfb9f54a
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-03-15 14:51:11 +0700

    added drawer blank

--- app/src/main/java/com/glogachev/twitchtopgames/view/MainActivity.kt
+++ app/src/main/java/com/glogachev/twitchtopgames/view/MainActivity.kt
@@ -3,16 +3,33 @@ package com.glogachev.twitchtopgames.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.glogachev.twitchtopgames.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navController = findNavController(R.id.nav_host_fragment_container)
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        toolbar.title = "Hello"
         setSupportActionBar(toolbar)
+        val drawer: DrawerLayout = findViewById(R.id.drawer_layout)
+
+        //appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.topGamesFragment),
+            drawerLayout = drawer
+        )
+        toolbar.setupWithNavController(
+            navController = navController,
+            configuration = appBarConfiguration
+        )
     }
 }
